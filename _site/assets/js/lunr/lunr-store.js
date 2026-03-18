f8a91016ddb161ef9958c921570bd7fba0b73ad0
Theo Queval <theoqueval@icloud.com>
2026-03-18 20:27:43 +0100

    V2 : Page projet voiture RC, nettoyage config et fix images

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Application de Gestion de Tâches",
-        "excerpt":"Présentation du projet  C’est ici que tu décris ton projet.   Ce que j’ai fait :     Authentification avec Firebase.   Gestion du state avec Redux.   Design responsive avec Tailwind CSS.                  Lien vers le site live       Code source sur GitHub          ","categories": [],
-        "tags": [],
-        "url": "/2026/03/04/mon-super-projet.html",
-        "teaser": "https://picsum.photos/400/300"
-      }]
